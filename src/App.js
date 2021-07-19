75dac673081986511e500cd3e712b5af7e297275
JoshuaLimZK <joshua_lim_zhe_kai@s2019.ssts.edu.sg>
2021-07-19 15:54:27 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://i1.sndcdn.com/artworks-RC6kOoicWfqBmolF-KmlnFQ-t500x500.jpg"
           target="_blank"
           rel="noopener noreferrer"
         >
