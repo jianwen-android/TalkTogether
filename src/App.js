a87bc0cf4c0161205d05e6aad2571e68c8e97cb9
jianwen <chin_jian_wen@s2019.ssts.edu.sg>
2021-07-19 16:58:30 +0800

    uh

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 export default function App() {
   return (
-/*
+    /*
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
