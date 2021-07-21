f7335b154052a6d5119c96bfd2b8fa5731cc385b
Irman Zaf <irman_zafyree_bin_irmanizam@s2019.ssts.edu.sg>
2021-07-21 08:43:29 +0800

    Testing pushing views with buttons

--- src/Pages/signIn.jsx
+++ src/Pages/signIn.jsx
@@ -1,3 +1,25 @@
-export default function SignInPage({ navigation }) {
-  return <text> Hello </text>;
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./home";
+import { useHistory } from 'react-router-dom';
+
+export default function SignInPage(){
+
+    handleClick = () => {
+        const history = useHistory();
+        console.log('Click happened');
+        history.push('/home');
+    }
+
+    return (
+        <div>
+            <h1> Sign In/Up Page Here</h1>
+            <button onClick={this.handleClick}> Go To Home </button>
+
+            <Switch>
+                <Route path="/home">
+                <Home />
+                </Route>
+            </Switch>
+        </div>
+    );
 }
