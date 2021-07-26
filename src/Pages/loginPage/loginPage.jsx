336ccf214b18a37693d32f388e41087edd6f1dae
Jian Wen <chin_jian_wen@s2019.ssts.edu.sg>
2021-07-26 20:29:27 +0800

    Installed material ui package Changed password box to obfuscate

--- src/Pages/loginPage/loginPage.jsx
+++ src/Pages/loginPage/loginPage.jsx
@@ -1,39 +1,43 @@
 import "./loginPage.css";
-import React from 'react';
-import aurora from './Images/aurora.svg';
-import logo from './Images/logo.svg';
+import React from "react";
+import aurora from "./Images/aurora.svg";
+import logo from "./Images/logo.svg";
 import { useHistory } from "react-router";
 
-export default function LoginPage(){
+export default function LoginPage() {
+  const history = useHistory();
 
-    const history = useHistory();
+  let emailInput = React.createRef();
+  let passwordInput = React.createRef();
 
-    let emailInput = React.createRef();
-    let passwordInput = React.createRef();
-  
-    function clicked() {
-      console.log("email: " + emailInput.current.value);
-      console.log("password: " + passwordInput.current.value);
-      history.push("/class");
-    }
+  function clicked() {
+    console.log("email: " + emailInput.current.value);
+    console.log("password: " + passwordInput.current.value);
+    history.push("/class");
+  }
 
-    return (
-        <body>
-            <img src={aurora} id='image' alt=''/>
-            <div id = 'main'>
-                <div id='secondary_container'>
-                    <div id="logins_container">
-                        <h1 id="title">Login</h1>
-                        <input class='input email' ref={emailInput} placeholder="Email"/>
-                        <input class='input password' ref={passwordInput} placeholder='Password'/>
-                        <button onClick={clicked} class='button login'>Login</button>
-                    </div>
-                    <div id='logo_container'>
-                        <img src={logo} id='logo' alt=''/>
-                    </div>
-                </div>
-            </div>
-        </body>
-    );
+  return (
+    <body>
+      <img src={aurora} id="image" alt="" />
+      <div id="main">
+        <div id="secondary_container">
+          <div id="logins_container">
+            <h1 id="title">Login</h1>
+            <input class="input email" ref={emailInput} placeholder="Email" />
+            <input
+              class="input password"
+              ref={"*".repeat(str.length)}
+              placeholder="Password"
+            />
+            <button onClick={clicked} class="button login">
+              Login
+            </button>
+          </div>
+          <div id="logo_container">
+            <img src={logo} id="logo" alt="" />
+          </div>
+        </div>
+      </div>
+    </body>
+  );
 }
-
