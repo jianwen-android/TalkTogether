42a1c660b766ca8f335c9c853a287a7eeb691720
Irman Zaf <irman_zafyree_bin_irmanizam@s2019.ssts.edu.sg>
2021-07-26 19:46:34 +0800

    bit of changes to login css

--- src/Pages/loginPage.jsx
+++ src/Pages/loginPage.jsx
@@ -1,8 +1,18 @@
 import "./loginPage.css";
+import React from 'react';
 import aurora from './Images/aurora.svg';
 import logo from './Images/logo.svg';
 
 export default function LoginPage(){
+
+    let emailInput = React.createRef();
+    let passwordInput = React.createRef();
+  
+    function clicked() {
+      console.log("email: " + emailInput.current.value);
+      console.log("password: " + passwordInput.current.value);
+    }
+
     return (
         <body>
             <img src={aurora} id='image' alt=''/>
@@ -10,12 +20,12 @@ export default function LoginPage(){
                 <div id='secondary_container'>
                     <div id="logins_container">
                         <h1 id="title">Login</h1>
-                        <input class='input email' placeholder="Email"/>
-                        <input class='input password' placeholder='Password'/>
-                        <button class='button login'>Login</button>
+                        <input class='input email' ref={emailInput} placeholder="Email"/>
+                        <input class='input password' ref={passwordInput} placeholder='Password'/>
+                        <button onClick={clicked} class='button login'>Login</button>
                     </div>
                     <div id='logo_container'>
-                        <img src={logo} id='image' alt=''/>
+                        <img src={logo} id='logo' alt=''/>
                     </div>
                 </div>
             </div>
